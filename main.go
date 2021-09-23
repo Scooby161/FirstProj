010549d41bc4bdc88449b725db94189829cd0785
Scooby <cthutq1597539@mail.ru>
2021-09-23 14:49:09 +0300

    First commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now() // время старта
+
+	fmt.Printf("Старт: %s\n", t.Format(time.RFC3339))
+
+	// очень сложные вычисления
+	go calculateSomething(1000)
+
+	// а эти еще сложнее
+	go calculateSomething(2000)
+
+	time.Sleep(32 * time.Second)
+
+	fmt.Printf("Время выполнения программы: %s\n", time.Since(t))
+}
+func calculateSomething(n int) {
+	t := time.Now()
+
+	go func() {
+		for {
+			for _, r := range `-\|/` {
+				fmt.Printf("\r%c", r)
+				time.Sleep(time.Millisecond * 1000)
+			}
+		}
+	}()
+
+	result := 0
+	for i := 0; i <= n; i++ {
+		result += i * 2
+		time.Sleep(time.Millisecond * 3)
+	}
+
+	fmt.Printf("Результат: %d; Прошло времени: %s\n", result, time.Since(t))
+}
